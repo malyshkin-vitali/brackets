d17e3c293f90cfc9f95ec3b82d8640b43f4d3cbb
Vitali Malyshkin <vitalyam13@gmail.com>
2020-10-10 22:12:27 +0300

    fix: correct src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let res = [];
+
+    for (let h = 0; h < bracketsConfig.length; h++) {
+
+        res.push(bracketsConfig[h][0] + bracketsConfig[h][1])
+
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (str.includes(res[i])) {
+
+            str = str.replace(res[i], "")
+            i = -1
+        }
+    }
+
+    if (str.length === 0) {
+        return true
+
+    } else {
+
+        return false
+    }
 }
